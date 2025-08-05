14af02858f4532233ed3c8cb1ace96c7a0146deb
Paulo Cesar Farias Junior <paulocesarfarias12345@gmail.com>
2025-08-05 12:18:49 -0300

    update
    update

---
+++ Reactjs/src/components/Card/index.jsx
@@ -0,0 +1,25 @@
+import posterImg from ''
+
+
+export function Card(){
+
+return(
+
+<div className="container">
+
+    <div>
+
+        <img src="" alt="star wars" />
+
+
+
+    </div>
+
+</div>
+
+);
+
+
+
+
+}
